5f18c0cf89f87643593ec7d5d0d301c81a1214e7
iamcrysun <adamova.01@mail.ru>
2023-04-19 18:52:21 +0300

    Custom window with frames

--- desktop/main.py
+++ desktop/main.py
@@ -1,11 +1,28 @@
-import ttkbootstrap
+from ttkbootstrap import Window
 
+from desktop.views.menu import Menu
+from desktop.views.registration import Registration
 from views.login import Login
 
-window = ttkbootstrap.Window(themename="darkly")
-window.title('Проверка трафика')
-window.geometry('1200x600')
 
-start_page = Login(window)
-start_page.pack()
-window.mainloop()
+class MainWindow(Window):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.login_page = Login(self)
+        self.registration_page = Registration(self)
+        self.menu_page = Menu(self)
+
+        self.current_frame = self.login_page
+        self.current_frame.pack()
+
+    def switch_frame(self, frame):
+        self.current_frame.destroy()
+        self.current_frame = frame
+        self.current_frame.pack()
+
+
+main_window = MainWindow()
+main_window.title("Проверка трафика")
+main_window.geometry('400x300')
+main_window.mainloop()
