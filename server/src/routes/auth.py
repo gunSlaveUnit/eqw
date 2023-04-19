2220da29cccecdb6083293772cc97a3529689cb8
iamcrysun <adamova.01@mail.ru>
2023-04-19 17:32:02 +0300

    All base auth routes

--- server/src/routes/auth.py
+++ server/src/routes/auth.py
@@ -3,6 +3,21 @@ from fastapi import APIRouter
 router = APIRouter(prefix='/auth')
 
 
+@router.post('/sign-up/')
+def sign_up():
+    return {"message": "Sign up"}
+
+
 @router.post('/sign-in/')
 def sign_in():
     return {"message": "Sign in"}
+
+
+@router.post('/sign-out/')
+def sign_out():
+    return {"message": "Sign out"}
+
+
+@router.post('/me/')
+def me():
+    return {"message": "Me"}
