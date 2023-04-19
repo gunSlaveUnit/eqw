964a90e9f5202968bba2d7dd4510a6694889e33e
iamcrysun <adamova.01@mail.ru>
2023-04-19 17:28:39 +0300

    Bound auth router

--- server/src/main.py
+++ server/src/main.py
@@ -3,13 +3,19 @@ import sys
 # TODO: bad
 sys.path.append("C:/eqw")
 
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 
 from server.src.utils.db import Base, engine
+from server.src.routes.auth import router as auth_router
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+router = APIRouter()
+router.include_router(auth_router)
+
+app.include_router(router)
 
 
 @app.get("/")
