2a566aa579a71986ede16ff6e901897ca456a227
iamcrysun <adamova.01@mail.ru>
2023-04-27 13:37:56 +0300

    add interpretation

---
+++ desktop/logic/interpretated.py
@@ -0,0 +1,39 @@
+import requests
+
+
+def interpretated(result, value):
+    att = list(set(result))
+    att.sort()
+    answer = "Вы столкнулись со следующими опасностями: "
+
+    reply = requests.get('http://localhost:23432/attack/')
+    attacks = reply.json()
+    print(attacks)
+    attack_titles = [attack['type'] for attack in attacks]
+
+    if value == 1:
+        if len(att) > 1:
+            if att[0] == 0:
+                # Добавляем к строке элемент списка
+                for i in range(1, len(att)):
+                    answer += attack_titles[att[i]]
+                    if i != len(att) - 1:
+                        answer += ', '
+            else:
+                for i in range(0, len(att)):
+                    answer += attack_titles[att[i]]
+                    answer += ', '
+        else:
+            if att[0] == 0:
+                answer = "Ваш трафик безопасен. Поздравляем!"
+            else:
+                answer = f"Вы столкнулись со следующей опасностью: {attack_titles[att[0]]}"
+    else:
+        if len(att) > 1:
+            answer = "Ваш трафик содержит вредоносные элементы"
+        else:
+            if att[0] == 0:
+                answer = "Ваш трафик безопасен. Поздравляем!"
+            else:
+                answer = "Ваш трафик содержит вредоносные элементы"
+    return (answer)
