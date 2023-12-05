a2915a79c199db2d6696f1e308d9913241b977f6
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-12-05 19:43:55 +0300

    Using of ICONS PATH for the icon. Some style refactoring

--- desktop/main.py
+++ desktop/main.py
@@ -1,26 +1,22 @@
-from tkinter import Button
 from tkinter.ttk import Notebook, Frame
 
-import requests
-import ttkbootstrap
+import ttkbootstrap as ttkb
 
-from ttkbootstrap import Window
-
-from desktop.logic.auth import exit_func
 from desktop.materials.strings import WINDOW_NAME
 from desktop.views.attackinfo import AttackInfo
 from desktop.views.check import Check
 from desktop.views.info import Info
-from desktop.views.menu import  AppMenu
+from desktop.views.menu import AppMenu
 from desktop.views.registration import Registration
 from desktop.views.settings import Settings
 from views.login import Login
+from settings import ICONS_PATH
 
 
-class MainWindow(Window):
+class MainWindow(ttkb.Window):
     def __init__(self):
         super(MainWindow, self).__init__()
-        ttkbootstrap.Style().theme_use('vapor')
+        ttkb.Style().theme_use('vapor')
 
         self.authorized_session = None
         self.current_user = None
@@ -41,18 +37,14 @@ class MainWindow(Window):
         self.current_frame.pack()
 
     def full_ui(self, tk=None):
-
-
         notebook = Notebook(self)
         notebook.pack(pady=10, expand=True)
 
         frame1 = Check(notebook)
         frame2 = Info(notebook)
-        frame3=AttackInfo(notebook)
+        frame3 = AttackInfo(notebook)
         frame4 = Settings(notebook)
         frame5 = Frame(notebook, width=780, height=580)
-
-
 
         frame1.pack(fill='both', expand=True)
         frame2.pack(fill='both', expand=True)
@@ -67,13 +59,9 @@ class MainWindow(Window):
         notebook.add(frame5, text='Справка')
 
 
-
-
-
-
 main_window = MainWindow()
 main_window.title(WINDOW_NAME)
 main_window.geometry('800x600')
-main_window.iconbitmap('C:\eqw\desktop\materials\icon.ico')
+main_window.iconbitmap(ICONS_PATH / 'shield.png')
 main_window.resizable(width=0, height=0)
 main_window.mainloop()
