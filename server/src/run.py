02b1b54830ee5565343b0ad8c1ff50c43c14dcb0
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-12-05 19:53:35 +0300

    Using HOST and PORT

--- server/src/run.py
+++ server/src/run.py
@@ -1,3 +1,6 @@
+import uvicorn
+
+from server.src.settings import HOST, PORT
+
 if __name__ == '__main__':
-    import uvicorn
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app:app", host=HOST, port=PORT, reload=True)
